bef4d30e61294d9f4c655677458d8a4b87be5d25
Oliver <olialexander08@gmail.com>
2023-07-04 18:01:49 +0300

    map generated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import MapTable from './Components/MapTable'
+import {useState, useEffect} from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[mapSchema, setMapSchema] = useState(null);
+  const[reRender, setReRender] = useState(0);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/maps")
+      .then(res => {
+        if (res.ok) {
+          return res.json(); 
+        } else {
+          throw new Error('Request failed with status: ' + res.status);
+        }
+      })
+      .then(data => {
+        setMapSchema(data);
+      })
+      .catch(error => console.error(error));
+  }, [reRender]);
+
+
+  const generateMapButtonEvent = () =>{
+    setReRender( reRender + 1);
+   //let grass = ["../public/grass2.png","../public/grass3.png", "../public/Group_1.png"];
+      //console.log(grass[Math.floor(Math.random() * grass.length)])
+   
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   { mapSchema &&  <MapTable size={32} classNameTable={"mapTable"} classNameTableBox={"mapBox"} mapArray={mapSchema.representation}/>}
+   <button onClick={generateMapButtonEvent} className='generateMapBtn'> Generate Map </button>
     </>
   )
 }
---
+++ frontend/src/Components/MapTable.jsx
@@ -0,0 +1,51 @@
+let grass = ["../public/grass2.png","../public/grass3.png", "../public/Group_1.png"];
+let mountain = ["../public/mountain.png"];
+let mineral = ["../public/minereu1.png", "../public/minereu2.png"];
+let water = ["../public/Water.png"];
+let pit = ["../public/pit.png"];
+
+export default function MapTable({size, classNameTable, classNameTableBox, mapArray}){
+   let rows = [];
+
+    for(let i = 0; i < size; i++){
+       rows.push(<tr key={`tableRow - ${i}`}>{createRow(size, classNameTableBox, i, mapArray)}</tr>)
+    }
+
+   
+    return <table className={classNameTable}><tbody>{rows}</tbody></table>;
+}
+
+
+
+const createRow = (size, classNameTableBox, rowNumber, mapArray) => {
+    let cels = [];
+
+   
+
+    for(let i = 0; i < size; i++){
+      
+        let imageForThisCell = getCorrectImage(mapArray, rowNumber, i)
+
+       cels.push(<td className={classNameTableBox}
+                     style={{backgroundImage : imageForThisCell}}
+                      key={`cell-${i}`}></td>)
+    }
+
+    return cels;
+   
+}
+
+const getCorrectImage = (mapArray, rowNumber, cellNumber) => {
+        if(mapArray[rowNumber][cellNumber] == "-"){
+            return `url(${grass[Math.floor(Math.random() * grass.length)]})`;
+        }else if(mapArray[rowNumber][cellNumber] == "#"){
+            console.log("mountain")
+            return`url(${mountain[Math.floor(Math.random() * mountain.length)]})`;
+        }else if(mapArray[rowNumber][cellNumber] == "&"){
+            return `url(${pit[Math.floor(Math.random() * pit.length)]})`;
+        }else if(mapArray[rowNumber][cellNumber] == "*"){
+            return `url(${water[Math.floor(Math.random() * water.length)]})`;
+        }else if(mapArray[rowNumber][cellNumber] == "%"){
+            return `url(${mineral[Math.floor(Math.random() * mineral.length)]})`;
+        }
+}
